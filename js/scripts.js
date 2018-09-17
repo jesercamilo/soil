6df848653abecb82d80e9811d28dc677b8f0cf27
Jese Camilo <jese.r.camilo@gmail.com>
2018-09-17 16:12:56 -0400

    made scrolling functionality persistent

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ $(function() {
 
 
 $('.carousel-slider').carousel({fullWidth: true, padding:0},setTimeout(autoplay, 4500));
-  function autoplay() {
+	function autoplay() {
     $('.carousel').carousel('next');
     setTimeout(autoplay, 7500);
   }
