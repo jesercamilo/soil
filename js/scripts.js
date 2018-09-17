fb627c5babafb2caf3a1b7ea9363824945f5cd09
Jese Camilo <jese.r.camilo@gmail.com>
2018-09-17 16:18:16 -0400

    more changes

--- js/scripts.js
+++ js/scripts.js
@@ -28,9 +28,8 @@ $(function() {
 
 
 $('.carousel-slider').carousel({fullWidth: true, padding:0},setTimeout(autoplay, 4500));
-	function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 7500);
-  }
-}(jQuery));
+function autoplay() {
+	$('.carousel').carousel('next');
+	setTimeout(autoplay, 7500);
+}}(jQuery));
 
